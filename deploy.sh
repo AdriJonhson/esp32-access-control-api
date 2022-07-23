487fc789e9bdd70122be1cd3f056f1e1a281ccf1
Adri Silva <adri.silva.elo@atos.com.br>
2022-07-22 23:59:09 -0300

    Update github config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
+userName=$1
+password=$2
+jdbcUrl=$3
+
+export DATABASE_USERNAME=userName
+export DATABASE_PASSWORD=password
+export DATABASE_JDBC_URL=jdbcUrl
+export JAVA_HOME=/root/.sdkman/candidates/java/current
+
 git pull origin main
-JAVA_HOME=/root/.sdkman/candidates/java/current
 ./mvnw clean package -Dmaven.test.skip
 docker compose up --build -d

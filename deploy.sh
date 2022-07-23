50a2828086719faf1ddc2c7144a82b0bc43d4fbb
Adri Silva <adri.silva.elo@atos.com.br>
2022-07-23 00:04:19 -0300

    Update github config

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ userName=$1
 password=$2
 jdbcUrl=$3
 
-export DATABASE_USERNAME=userName
-export DATABASE_PASSWORD=password
-export DATABASE_JDBC_URL=jdbcUrl
+export DATABASE_USERNAME=$userName
+export DATABASE_PASSWORD=$password
+export DATABASE_JDBC_URL=$jdbcUrl
 export JAVA_HOME=/root/.sdkman/candidates/java/current
 
 git pull origin main
